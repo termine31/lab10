572b223ab982b493e7ba375d1e04d12e90e87604
termine1 <gsafdq.tn.qe12@gmail.com>
2026-03-01 12:26:17 +0700

    Создаю длину массива N Проверяю на условие что n>>1000 Создаю дин.массив и заполняю его рандом числами от 0-17 с помощью функции rand и сортирую пузырьковой сортировкой Далее создаю счетчик, который будет считать сколько раз появлялось каждое число от 0-17 Функция ypac: Считает сколько раз каждое число появлялось Сначала заполняет массив счетчика нулями, потом через цикл считает скок попадается каждое число. Вывожу исходный массив, потом вывожу упакованный массив: Число - сколько раз попадается

--- lab10/lab10.cpp
+++ lab10/lab10.cpp
@@ -1,8 +1,66 @@
 ﻿
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+void ypac(int* mass1, int len, int* cc);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Введите длину массива(n>1000): \n"; 
+    int n;
+    cin >> n; 
+    if (n < 1000) {
+        cout << "Длина массива меньше 1000 :( ";
+        return 0;
+    }
+    int* mass = new int[n];
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        mass[i] = rand() % 17 + 0;
+    }
+    int temp;
+    // отсортировка 
+    for (int i = 0; i < n - 1; i++) {
+        for (int j=0;j<n-i-1;j++){
+            if (mass[j] > mass[j + 1]) {
+                temp = mass[j];
+                mass[j] = mass[j + 1];
+                mass[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Исходный массив чисел: \n";
+    for (int i = 0; i < n; i++) {
+        cout << mass[i] << " "; 
+    }
+    cout << endl;
+    // ПУНКТ А
+    //массив счетчик
+    int count[18];
+    ypac(mass, n, count);
+    int tcount = 0;
+    cout << "-------------------------------------------------------------------------------------------\n";
+    cout << "Упакованный массив чисел: \n";
+    // вывод чисел которые есть в массиве
+    for (int i = 0; i < 18; i++) {
+        if (count[i] > 0) {
+            cout << "Число - " << i << " | " << "Попадается количество раз: " << count[i] << endl;;
+            tcount += count[i];
+        }
+    }
+    delete[] mass;
+    return 0;
 }
 
+void ypac(int* mass1, int len, int* cc) {
+    for (int i = 0; i < 18; i++) {
+        cc[i] = 0;
+    }
+    for (int i = 0; i < len; i++) {
+        int val = mass1[i];
+        cc[val]++;
+    }
+}
